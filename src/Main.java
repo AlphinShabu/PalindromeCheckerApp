92c7d1fd3ae609e5210cfb78797bdd0190007cae
AlphinShabu <alphin0812@yahoo.com>
2026-02-23 15:25:47 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" Welcome to Palindrome Checker System ");
+        System.out.println("========================================");
+        System.out.println("Version: 1.0");
+        System.out.println("Application started successfully!");
+    }
+}
