8071a43e4eeac5e45d1001ccfd36907705d2781f
AlphinShabu <alphin0812@yahoo.com>
2026-02-23 15:54:54 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class UseCase1PalindromeCheckerApp {
+import java.util.Scanner;
+
+class UseCase2PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("========================================");
         System.out.println(" Welcome to Palindrome Checker System ");
         System.out.println("========================================");
-        System.out.println("Version: 1.0");
-        System.out.println("Application started successfully!");
+        System.out.println("Version: 2.0");
+
+        System.out.print("Enter a word: ");
+        String input = scanner.nextLine();
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("Result: \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("Result: \"" + input + "\" is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
